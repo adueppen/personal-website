95f5cf39cda980a1c84442c3aec8c7596ab1e7d6
Alex Dueppen <alex@adueppen.xyz>
2019-08-21 19:59:14 -0500

    whoops forgot to passthrough the manifest
    also reformatted stuff for 2 spaces

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,16 @@
 const pluginPWA = require("eleventy-plugin-pwa");
 
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPlugin(pluginPWA);
-    eleventyConfig.addPassthroughCopy('src/images')
-    /*todo: next release of 11ty will support copy passthrough globbing so I could put favicon.ico and manifest.json
-       in a different folder and make things less weird*/
-    return {
-        dir: { input: 'src', output: 'dist', data: '_data' },
-        passthroughFileCopy: true,
-        templateFormats: ['njk', 'md', 'css', 'html', 'yml', 'ico'],
-        htmlTemplateEngine: 'njk'
-    }
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginPWA);
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/manifest.json");
+  /*todo: next release of 11ty will support copy passthrough globbing so I could put favicon.ico and manifest.json
+     in a different folder and make things less weird*/
+  return {
+    dir: {input: "src", output: "dist", data: "_data"},
+    passthroughFileCopy: true,
+    templateFormats: ["njk", "md", "css", "html", "yml"],
+    htmlTemplateEngine: "njk"
+  }
 };
