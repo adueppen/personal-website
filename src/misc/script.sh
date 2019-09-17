13409735bf4dbe229f9e3789b8dc298c2264a1cd
Alex Dueppen <me@ajd.sh>
2019-09-17 17:43:05 -0500

    More CSS work plus make shell script better

--- src/misc/script.sh
+++ src/misc/script.sh
@@ -1,1 +1,6 @@
-echo "Here's the shell script for my little gimmick. It's not much yet, but I'll try to make it do some fun stuff."
+if ! command -v cowsay > /dev/null; then
+  echo "You don't even have cowsay installed‽ Impossible..."
+else
+  echo "Welcome to my secret shell script! It's not much yet but I'll try to make it do some fun stuff." | \
+  cowsay -f "milk"
+fi
