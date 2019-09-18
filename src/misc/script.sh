3eca59e6019e9c50fd0c0df6de55d2d98f9c879e
Alex Dueppen <me@ajd.sh>
2019-09-17 19:41:13 -0500

    A few random changes

--- src/misc/script.sh
+++ src/misc/script.sh
@@ -1,5 +1,6 @@
 if ! command -v cowsay > /dev/null; then
-  echo "You don't even have cowsay installed‽ Impossible..."
+  echo "You don't have cowsay installed?! Impossible..."
+  echo "Please install it and try again."
 else
   echo "Welcome to my secret shell script! It's not much yet but I'll try to make it do some fun stuff." | \
   cowsay -f "milk"
