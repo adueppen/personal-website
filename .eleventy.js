d609fadbde585973b035c12723f8b9bf7af60791
Alex Dueppen <me@ajd.sh>
2019-10-30 13:11:04 -0500

    Change minifier to only activate in production build

--- .eleventy.js
+++ .eleventy.js
@@ -5,16 +5,18 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginPWA);
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy({"src/misc": "/"});
-  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-    if (outputPath.endsWith(".html")) {
-      return htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true
-      });
-    }
-    return content;
-  }); //TODO: make this transform only activate in the production build
+  if (process.env.NODE_ENV === "prod") {
+    eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+      if (outputPath.endsWith(".html")) {
+        return htmlmin.minify(content, {
+          useShortDoctype: true,
+          removeComments: true,
+          collapseWhitespace: true
+        });
+      }
+      return content;
+    });
+  }
   return {
     dir: {input: "src", output: "dist", data: "_data"},
     passthroughFileCopy: true,
