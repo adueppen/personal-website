37a94c3c79cdf0c82cbc99e27cdbd9b1f23c1531
Alex Dueppen <me@ajd.sh>
2019-10-30 12:40:27 -0500

    Social link refactor, npm package updates, add HTML minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,20 @@
 const pluginPWA = require("eleventy-plugin-pwa");
+const htmlmin = require("html-minifier");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginPWA);
   eleventyConfig.addPassthroughCopy("src/images");
-  eleventyConfig.addPassthroughCopy({"src/misc" : "/"});
+  eleventyConfig.addPassthroughCopy({"src/misc": "/"});
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    if (outputPath.endsWith(".html")) {
+      return htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+    }
+    return content;
+  }); //TODO: make this transform only activate in the production build
   return {
     dir: {input: "src", output: "dist", data: "_data"},
     passthroughFileCopy: true,
